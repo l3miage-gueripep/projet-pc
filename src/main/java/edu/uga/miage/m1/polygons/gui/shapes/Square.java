25e28abe9b6b15cda569c98ff5f8a2ea605d8f08
paul <gueripel.paul@gmail.com>
2023-12-06 12:49:41 +0100

    new branch

--- src/main/java/edu/uga/miage/m1/polygons/gui/shapes/Square.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/shapes/Square.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package edu.uga.miage.m1.polygons.gui.shapes;
 
 import java.awt.BasicStroke;
@@ -26,12 +8,7 @@ import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
 import edu.uga.miage.m1.polygons.gui.persistence.Visitor;
 
-/**
- * This class implements the square <tt>SimpleShape</tt> extension.
- * It simply provides a <tt>draw()</tt> that paints a square.
- *
- * @author <a href="mailto:christophe.saint-marcel@univ-grenoble-alpes.fr">Christophe</a>
- */
+
 public class Square extends SimpleShape {
     private Rectangle2D rectangle;
 
@@ -40,11 +17,7 @@ public class Square extends SimpleShape {
         this.rectangle = new Rectangle2D.Double(this.x, this.y, size, size);
     }
 
-    /**
-     * Implements the <tt>SimpleShape.draw()</tt> method for painting
-     * the shape.
-     * @param panelGraphics2d The graphics object used for painting.
-     */
+
     public void draw(Graphics2D g2) {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         GradientPaint gradient = new GradientPaint(x, y, Color.BLUE, (float) x + size, y, Color.WHITE);
